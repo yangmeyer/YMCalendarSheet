348d17aeb817edb372ec43e5549cd982e0ff3389
Yang Meyer <mail@yangmeyer.de>
2012-07-09 13:08:01 -0400

    Initial Commit

---
+++ YMCalendarSheetDemo/YMAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  YMAppDelegate.h
+//  YMCalendarSheetDemo
+//
+//  Created by Yang Meyer on 09.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class YMViewController;
+
+@interface YMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) YMViewController *viewController;
+
+@end
---
+++ YMCalendarSheetDemo/YMViewController.h
@@ -0,0 +1,13 @@
+//
+//  YMViewController.h
+//  YMCalendarSheetDemo
+//
+//  Created by Yang Meyer on 09.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMViewController : UIViewController
+
+@end
